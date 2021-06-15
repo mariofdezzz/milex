81afef762322f4944a72ff76f0bc50845408456e
mariofdezzz <marioferrerofdez@gmail.com>
2021-06-15 14:37:49 +0100

    ejemplo completo

--- tests/ejgc.f
+++ tests/ejgc.f
@@ -1,23 +1,7 @@
 
-int p
-
-
-int fibo () {
-    int j = 0
-    int k = 1
-
-    for (int i=0; i<25; i=i+1) {
-        println(j)
-
-        int temp = k
-        k = j + k
-        j = temp
-    }
-
-    return j
-}
-
 {   
-    println(fibo())
+    int a = 22
+
+    println(a)
 }
 
