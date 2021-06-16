c9afc50d74d2c2ca3fc4fdbf89e254adc6d95db5
mariofdezzz <marioferrerofdez@gmail.com>
2021-06-16 17:28:53 +0100

    arreglo test

--- test/ejemplo.m
+++ test/ejemplo.m
@@ -1,3 +1,21 @@
-int sort() {
-    int result = 2
+
+int f() {
+    int x = 25
+
+    for ( 
+        int a = -1; 
+        a < 5; 
+        a = ++a
+    ) {
+        if (a > 4) {
+            println(a)
+        }else{println(x)}
+    }
+
+    return 2
 }
+
+{
+    f()
+}
+
