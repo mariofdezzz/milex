4c40ee7da358740689ec4599652d229cf3f21ff4
qwerteleven <ll11ll1@outlook.es>
2021-06-22 23:17:36 +0100

    Create ejemplo-while.m

---
+++ test/ejemplo-while.m
@@ -0,0 +1,23 @@
+int y
+
+{ 
+
+    int a
+
+    a = -1
+
+    while (a > 7 + 8) {
+        a = ++a
+        println(a)
+
+    }
+
+    a = 5
+
+    while (a < 7 + 8) {
+        a = ++a
+        println(a)
+
+    }
+
+}
